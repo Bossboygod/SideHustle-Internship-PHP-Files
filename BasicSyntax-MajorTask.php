999a593020ecf0792ce410f08f1cb267a8ac18ab
Ogundeyi Joshua <60978579+Bossboygod@users.noreply.github.com>
2020-10-15 17:54:05 +0100

    Add files via upload

---
+++ BasicSyntax-MajorTask.php
@@ -0,0 +1,22 @@
+<?php 
+
+//======= first major task.======
+
+/*
+Occupation: Student
+Gender: Male
+Sidehustle Username: Jayyy
+*/
+
+//about me tag
+echo "<h1>About me</h1><hr>";
+
+//my side hustle internship username
+echo "<h3>Jayyy</h3>";
+
+//about myself
+echo "<p>I am a Metallurgical and Materials engineering student in the University of Lagos. 
+         I am an analytical individual with vast knowledge in programming. 
+         My hobbies are gaming, coding, listening to music</p>";
+
+?>
